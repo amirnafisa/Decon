d02ed9e0448cd6bda9752ec17b24185a4e767e32
Nafisa Ali Amir <amirnafisa@gmail.com>
2020-04-14 14:44:54 -0400

    Updated to return box centers and labels

--- scripts/detect.py
+++ scripts/detect.py
@@ -7,7 +7,7 @@ import time
 import sys
 
 model_file = "/home/nvidia/deconbot/src/turtlebot3/turtlebot3_bringup/scripts/model_test2.pth"
-threshold = 0.5
+threshold = 0.7
 
 def prepare_model():
 
@@ -49,11 +49,11 @@ def detect_image(imagePath, model, tr):
 	    scores = output["scores"].cpu()
 	    for box, score, label in zip(output_boxes.tolist(), scores.tolist(), labels.tolist()):
 	        if score > threshold:
-	            pred_boxes.append(box)
+	            pred_boxes.append([(box[0]+box[2])/2, (box[1]+box[3])/2, box[2]-box[0], box[3]-box[1]])
 	            pred_labels.append(label)
 	
 	del model
 	del image
 	del device
 	torch.cuda.empty_cache()
-	return pred_boxes
+	return pred_boxes, pred_labels
