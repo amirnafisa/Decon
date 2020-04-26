2f23e99fa070b1a6bbcbbdcd787153f9615f714d
Nafisa Ali Amir <amirnafisa@gmail.com>
2020-04-26 19:10:34 -0400

    After calibrating arm with controller with Neel

--- darm_interface.py
+++ darm_interface.py
@@ -1,6 +1,33 @@
 import rospy
 
 scale = 0.1
+min_z = 0.1
+max_z = 1.0
+min_x = -0.1
+max_x = 0.5
+min_y = -0.5
+max_y = 0.5
+
+def check_bounds(pose):
+	if pose.position.x < min_x:
+		print("[Decon]: X less than minX...")
+		return False
+	if pose.position.x > max_x:
+		print("[Decon]: X greater than maxX...")
+		return False
+	if pose.position.y < min_y:
+		print("[Decon]: y less than minY...")
+		return False
+	if pose.position.y > max_y:
+		print("[Decon]: y greater than maxY...")
+		return False
+	if pose.position.z < min_z:
+		print("[Decon]: z less than minZ...")
+		return False
+	if pose.position.z > max_z:
+		print("[Decon]: z greater than maxZ...")
+		return False
+	return True
 
 def move_to_pose(group, rate, waypoints):
 	(plan, fraction) = group.compute_cartesian_path(waypoints, 0.01, 0.0)
@@ -14,6 +41,8 @@ def inc_z(group, rate):
 	wpose = group.get_current_pose().pose
         wpose.position.z += scale
 	wpose.orientation.w = 1
+	if not check_bounds(wpose):
+		return
 	move_to_pose(group, rate, [wpose])
         rate.sleep()
 
@@ -22,6 +51,8 @@ def dec_z(group, rate):
 	wpose = group.get_current_pose().pose
 	wpose.position.z -= scale
 	wpose.orientation.w = 1
+	if not check_bounds(wpose):
+		return
 	move_to_pose(group, rate, [wpose])
 	rate.sleep()
 
@@ -30,6 +61,8 @@ def inc_x(group, rate):
         wpose = group.get_current_pose().pose
         wpose.position.x += scale
  	wpose.orientation.w = 1
+	if not check_bounds(wpose):
+		return
         move_to_pose(group, rate, [wpose])
 	rate.sleep()
 
@@ -38,6 +71,8 @@ def dec_x(group, rate):
         wpose = group.get_current_pose().pose
         wpose.position.x -= scale
         wpose.orientation.w = 1
+	if not check_bounds(wpose):
+		return
         move_to_pose(group, rate, [wpose])
 	rate.sleep()
 
@@ -46,6 +81,8 @@ def inc_y(group, rate):
         wpose = group.get_current_pose().pose
         wpose.position.y += scale
         wpose.orientation.w = 1
+	if not check_bounds(wpose):
+		return
         move_to_pose(group, rate, [wpose])
 	rate.sleep()
 
@@ -54,11 +91,20 @@ def dec_y(group, rate):
         wpose = group.get_current_pose().pose
         wpose.position.y -= scale
         wpose.orientation.w = 1
+	if not check_bounds(wpose):
+		return
         move_to_pose(group, rate, [wpose])
 	rate.sleep()
 
 def move(group, rate, x_offset, y_offset, z_offset):
 	
+	#Z offset
+	while z_offset >= scale:
+		inc_z(group, rate)
+		z_offset -= scale
+	while z_offset*-1 >= scale:
+		dec_z(group, rate)
+		z_offset += scale
 	#X offset
 	while x_offset >= scale:
 		inc_x(group, rate)
@@ -73,11 +119,5 @@ def move(group, rate, x_offset, y_offset, z_offset):
 	while y_offset*-1 >= scale:
 		dec_y(group, rate)
 		y_offset += scale
-	#Z offset
-	while z_offset >= scale:
-		inc_z(group, rate)
-		z_offset -= scale
-	while z_offset*-1 >= scale:
-		dec_z(group, rate)
-		z_offset += scale
+
 	
